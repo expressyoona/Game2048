ecc1fe94e14cc4c1186bb2c409f5a8c9a0bf88ac
hoang <nbmhoang.17it1@sict.udn.vn>
2019-05-14 16:20:13 +0700

    Update features

--- app/src/main/java/com/example/game2048/MainActivity.java
+++ app/src/main/java/com/example/game2048/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.game2048;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,10 +24,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         hugeButton = (Button) findViewById(R.id.hugeButton);
         hugeButton.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
-
+        Toast.makeText(this, "Clicked button", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, NormalGame.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/game2048/NormalGame.java
+++ app/src/main/java/com/example/game2048/NormalGame.java
@@ -1,21 +1,161 @@
 package com.example.game2048;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Vector;
 
 public class NormalGame extends AppCompatActivity {
 
     Button[][] board;
+    int[][] arr;
     int n = 4;
     Random random;
-    Vector<String> emptyCell;
+    TextView txtScore;
+    long score;
 
-    public int getRandom(int a, int b) {
-        return random.nextInt(b) - random.nextInt(a) + 1;
+
+    private int min_distance = 100;
+    private float downX, downY, upX, upY;
+    View view;
+
+    private float x1, x2, y1, y2;
+    static final int MIN_DISTANCE = 150;
+    Toast t;
+
+    HashMap<String, Integer> color;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            }
+            case MotionEvent.ACTION_UP:  {
+                x2 = event.getX();
+                y2 = event.getY();
+                float deltaX = x2 - x1;
+                float deltaY = y2 - y1;
+                if (Math.abs(deltaX) > MIN_DISTANCE) {
+                    if (x1 > x2) {
+                        moveLeft();
+                    } else if (x1 < x2) {
+                        moveRight();
+                    }
+                } else  if (Math.abs(deltaY) > MIN_DISTANCE){
+                    if (y1 > y2) {
+                        moveUp();
+                    } else if (y1 < y2) {
+                        moveDown();
+                    }
+                }
+                break;
+            }
+
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void moveCellsUp() {
+        for(int j = 0;j < n;j++) {
+            stack(j);
+            merge(j);
+            stack(j);
+        }
+    }
+
+    public void moveRight() {
+        System.out.println("Moved right");
+        rotate270();
+
+        moveCellsUp();
+
+        rotate90();
+        refresh();
+        generate();
+
+        view();
+    }
+
+    public void moveLeft() {
+        System.out.println("Moved left");
+        rotate90();
+
+        moveCellsUp();
+
+        rotate270();
+        refresh();
+        generate();
+        view();
+    }
+
+    public void moveDown() {
+        System.out.println("Moved down");
+        rotate180();
+
+        moveCellsUp();
+
+        rotate180();
+        refresh();
+        generate();
+        view();
+    }
+
+    private void view() {
+        String s = "";
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                if (arr[i][j] != 0) {
+                    s += arr[i][j];
+                } else {
+                    s += '0';
+                }
+            }
+            s += '|';
+        }
+        System.out.println(s);
+    }
+
+    public void moveUp() {
+        System.out.println("Moved up");
+        //Move Cell Ups
+        moveCellsUp();
+
+        refresh();
+        generate();
+        view();
+    }
+
+    public void refresh() {
+
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                board[i][j].setBackgroundColor(color.get(String.valueOf(arr[i][j])));
+                if (arr[i][j] == 0) {
+                    board[i][j].setText("");
+                } else {
+                    board[i][j].setText(arr[i][j] + "");
+
+                }
+            }
+        }
+    }
+
+    public int getRandom(int min, int max) {
+        return random.nextInt((max - min + 1)) + min;
     }
 
     public boolean isFull() {
@@ -29,10 +169,118 @@ public class NormalGame extends AppCompatActivity {
         return false;
     }
 
+    public void generate() {
+        while(true) {
+            int r = this.getRandom(0, n - 1);
+            int c = this.getRandom(0, n - 1);
+            if (board[r][c].getText().equals("")) {
+                //int v = this.getRandom(1, 2) * 2;
+                board[r][c].setText("2");
+                arr[r][c] = 2;
+                return ;
+            }
+        }
+        /*String s = "";
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                if (arr[i][j] != 0) {
+                    s += arr[i][j];
+                } else {
+                    s += '0';
+                }
+            }
+            s += '|';
+        }
+        System.out.print(s);*/
+    }
+
+    public int findNotZero(int i, int j) {
+        for(int k = i - 1;k >= 0;k--) {
+            if (arr[k][j] > 0) {
+                return k;
+            }
+        }
+        return -1;
+    }
+
+    public void stack(int j) {
+        for(int i = 1;i < n;i++) {
+            if (arr[i][j] > 0) {
+                int k = this.findNotZero(i, j);
+                if (k < (i - 1)) {
+                    arr[k+1][j] = arr[i][j];
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void merge(int j) {
+        for(int i = 0;i < n - 1;i++) {
+            if (arr[i][j] == arr[i+1][j]) {
+                arr[i][j] *= 2;
+                score += arr[i][j];
+                arr[i+1][j] = 0;
+            }
+        }
+        txtScore.setText(String.valueOf(score));
+    }
+
+    public void rotate90() {
+        int[][] tempArr = new int[n][n];
+        for(int i = 0;i < n;++i) {
+            for(int j = 0;j < n;++j) {
+                tempArr[i][j] = arr[n - j - 1][i];
+            }
+        }
+        arr = tempArr;
+    }
+
+    public void rotate180() {
+        int[][] tempArr = new int[n][n];
+        for(int i = 0;i < n;++i) {
+            for(int j = 0;j < n;++j) {
+                tempArr[i][j] = arr[n - i - 1][n - j - 1];
+            }
+        }
+        arr = tempArr;
+    }
+
+    public void rotate270() {
+        int[][] tempArr = new int[n][n];
+        for(int i = 0;i < n;++i) {
+            for(int j = 0;j < n;++j) {
+                tempArr[i][j] = arr[j][n - i - 1];
+            }
+        }
+        arr = tempArr;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_game);
+
+        board = new Button[n][n];
+        random = new Random();
+        txtScore = findViewById(R.id.scorce);
+        txtScore.setText("0");
+        score = 0;
+
+        color = new HashMap<>();
+        color.put("0", ContextCompat.getColor(getApplicationContext(), R.color._0point));
+        color.put("2", ContextCompat.getColor(getApplicationContext(), R.color._2point));
+        color.put("4", ContextCompat.getColor(getApplicationContext(), R.color._4point));
+        color.put("8", ContextCompat.getColor(getApplicationContext(), R.color._8point));
+        color.put("16", ContextCompat.getColor(getApplicationContext(), R.color._16point));
+        color.put("32", ContextCompat.getColor(getApplicationContext(), R.color._32point));
+        color.put("64", ContextCompat.getColor(getApplicationContext(), R.color._64point));
+        color.put("128", ContextCompat.getColor(getApplicationContext(), R.color._128point));
+        color.put("256", ContextCompat.getColor(getApplicationContext(), R.color._256point));
+        color.put("512", ContextCompat.getColor(getApplicationContext(), R.color._512point));
+        color.put("1024", ContextCompat.getColor(getApplicationContext(), R.color._1024point));
+        color.put("2048", ContextCompat.getColor(getApplicationContext(), R.color._2048point));
+
 
         board[0][0] = (Button) findViewById(R.id.cell_1);
         board[0][1] = (Button) findViewById(R.id.cell_2);
@@ -54,7 +302,26 @@ public class NormalGame extends AppCompatActivity {
         board[3][2] = (Button) findViewById(R.id.cell_15);
         board[3][3] = (Button) findViewById(R.id.cell_16);
 
+        arr = new int[n][n];
 
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n;j++) {
+                board[i][j].setText("");
+                board[i][j].setBackgroundColor(color.get("0"));
+                arr[i][j] = 0;
+            }
+        }
 
+        for(int i = 0;i < 2;) {
+            int r = this.getRandom(0, n - 1);
+            int c = this.getRandom(0, n - 1);
+
+            if (board[r][c].getText().equals("")) {
+                i++;
+                board[r][c].setText("2");
+                arr[r][c] = 2;
+            }
+        }
     }
+
 }
